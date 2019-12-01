19685684822746f2f349cfcaa830c8035e54f6e0
Bert Douglas <georgehdouglas@gmail.com>
2019-12-01 01:38:12 -0600

    update

--- test.sh
+++
@@ -1,8 +0,0 @@
-
-#  -r:System.Collections.Generic.dll       \
-#  -r:System.Text.dll                      \
-#  -r:System.Text.RegularExpressions.dll   \
-
-csc -debug                                \
-  logic.cs                                \
-  logic-console.cs
